f681570736545d7f11bcac483394302d51f1109c
Ashutosh Kumar <ashutoshbro25@gmail.com>
2026-09-11 10:57:56 +0530

    Connect contact form to live backend

--- script.js
+++ script.js
@@ -201,7 +201,7 @@ if (contactForm) {
         try {
 
             const response = await fetch(
-                "http://localhost:5000/api/leads",
+                "https://nexttech-backend-euka.onrender.com/api/leads",
                 {
                     method: "POST",
                     headers: {
